df45e39f3e5c897566780d61024fe046fb6d5b19
Tina Dessu <tinady2009@gmail.com>
2023-04-30 00:09:34 +0300

    Update MenuLink.jsx

--- reduxtrial/src/components/menuLink/MenuLink.jsx
+++ reduxtrial/src/components/menuLink/MenuLink.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./menulink.css";
 
-export default function MenuLink({ icon, text }) {
+export default function MenuLink({  text }) {
   return (
     <div className="menulink">
-      {icon}
+     
       <span className="menuLinkText">{text}</span>
       <span className="menuLinkTextName">
         {text === "Logout" && "( John )"}
